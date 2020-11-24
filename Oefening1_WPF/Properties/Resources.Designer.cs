f21b185fcc8bbccc9b558c41d0c08dd3e9eef311
hcopp <hcopp@DESKTOP-SF67LSQ>
2020-11-24 12:33:39 +0100

    Oplossing Oefening 1 WPF

--- Oefening1_WPF/Properties/Resources.Designer.cs
+++ Oefening1_WPF/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace HalloWPFApp.Properties {
+namespace Oefening1_WPF.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace HalloWPFApp.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("HalloWPFApp.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Oefening1_WPF.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
@@ -57,46 +57,6 @@ namespace HalloWPFApp.Properties {
             }
             set {
                 resourceCulture = value;
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap C4koydo {
-            get {
-                object obj = ResourceManager.GetObject("C4koydo", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap hco {
-            get {
-                object obj = ResourceManager.GetObject("hco", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap Hedwig {
-            get {
-                object obj = ResourceManager.GetObject("Hedwig", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap MetJenny {
-            get {
-                object obj = ResourceManager.GetObject("MetJenny", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
             }
         }
     }
